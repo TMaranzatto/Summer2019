b2588d7d62e72d2917feb86a773b75dbc216ab96
Thomas Maranzatto <thomas.maranzatto16@ncf.edu>
2019-06-11 19:23:38 -0400

    small changes

--- LinkedConcurrent/LinkedConcurrent/Window.h
+++ LinkedConcurrent/LinkedConcurrent/Window.h
@@ -5,6 +5,7 @@
 struct node {
 	node* next;
 	int keyvalue;
+	bool mark;
 	//key = std::hash<std::T>()(data);
 };
 
@@ -16,14 +17,34 @@ class Window {
 	}
 };
 
-Window find(node* head, int key) {
+Window* find(node* head, int key) {
 	node* pred = NULL;
 	node* curr = NULL;
 	node* succ = NULL;
 	bool marked[] = { false };
 	bool snip;
-	while (true) {
+	label: while (true) {
 		pred = head;
-		curr = head -> next;
+		curr = head->next;
+		while (true) {
+			//fill with get(marked) once implemented
+			succ = curr->next;
+			while (marked[0]) {
+				//fill with compare and set
+				snip = pred->mark;
+				if (!snip) goto label;
+				curr = succ;
+				//replace with get statement
+				succ = curr -> next;
+			}
+			if (curr -> keyvalue >= key) {
+				//pretty sure right now the control flow is blocking
+				//code from executing here.  Once we have atomic updates
+				//this should be executable
+				return new Window(pred, curr);
+			}
+			pred = curr;
+			curr = succ;
+		}
 	}
 }
