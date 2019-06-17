cf11ddf6dfa35d2acc5843c8d8e7c1b4c50a865c
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2019-06-17 18:56:59 -0400

    Add files via upload
    Scrubbed my bad code :)

---
+++ Experiment/CLL/test.cpp
@@ -0,0 +1,14 @@
+#include "cll.h"
+//#include <iostream>
+
+using namespace std; 
+
+int main() {
+	//cll <char> test; 
+	//new cll<int>; 
+	//bool b = test.search("a");
+	//test.insert("b");
+	cout << "WORKING";
+
+	return 0; 
+}
