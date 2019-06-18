63c1f5d1ab19bc077559be4cb9e298647b5846f1
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2019-06-17 20:32:52 -0400

    Add files via upload
    Got the Base for 2 more experiments to run :)

---
+++ Experiment/CLL/Heap.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Heap {
+private: 
+
+
+	vector<int> heaps; 
+
+
+
+public: 
+	Heao() {
+
+
+	}
+	~Heap() {
+		if (heaps) {
+			delete(heaps)
+		}
+
+	}
+
+
+
+
+
+};
