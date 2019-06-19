85b37740038cd49b6309a7b1a520f1961ccac5c8
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2019-06-19 13:02:07 -0400

    Add files via upload
    Initialization of a Very Simple allocator...This being Free and Malloc

---
+++ Experiment/Simpleallocator.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+
+
+class Simpleallocator {
+private: 
+
+
+
+
+
+public: 
+
+
+	Simpleallocator() {
+
+
+	}
+
+	~Simpleallocator() {
+
+
+
+	}
+	
+	void* mmalloc(size_t size) {
+
+
+
+	}
+
+	void free(void* block) {
+
+
+	}
+
+	void set_heap() {
+
+
+	}
+
+	void freenode() {
+
+
+	}
+
+
+};
