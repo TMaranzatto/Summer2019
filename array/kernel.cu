5def9ce13cb17658c80ceecdae92f3c250036f84
Thomas Maranzatto <thomas.maranzatto16@ncf.edu>
2019-07-15 18:26:03 -0400

    working on insert method

--- array/kernel.cu
+++ array/kernel.cu
@@ -4,13 +4,64 @@
 
 #include <stdio.h>
 
-//returns value for threadId%64 if not taken
+struct arrayNode{
+	int[64] array;
+	unsigned long long int bitmap;
+	int min;
+	int max;
+	arrayNode* next;
+
+}
+
+//this implementation assumes a uniform distro of keys
+//as well as naiive partitions
+//no merging implemented yet
+//assume value is within some reasonable range
+__global__
+void insert(arrayNode *start, int value){
+	//finding the location to insert
+	arrayNode *current = *start;
+	int jumps[resolution] = { 3, 5, 7, 11, 13, 17, 19, 23 };
+
+	while(current->next != NULL){
+		
+
+		else if((current->min) <= value && value <= (current->max)){
+			for(int i = 0; i < 64; i++){
+				if (current->bitmap != ULLONG_MAX){
+
+					int jump = jumps[threadIdx.x % resolution];
+					unsigned long long int index = (blockIdx.x * blockDim.x + threadIdx.x + jump) % 64;
+					unsigned long long int loc = 1 << i;
+					unsigned long long int previousValue = atomicOr(bitmap, loc);
+
+					if ((previousValue >> i) & 1 == 0) {
+
+						current->array[index] = value;
+					}
+					else{
+
+						continue;
+					}
+				}
+				else{
+
+					continue;
+				}
+			}
+		}
+		else{
+			*current = current->next;
+		}
+	}
+
+
+}
+
+//returns "random" value for threadId%64 if not taken
 //else returns NULL
 //assumes array is for integers for simplicity
 //can template this later
-
-
-
 __global__
 void get(int* array, unsigned long long int* bitmap) {
 	printf("your thread is %d.%d.\n", blockIdx.x * blockDim.x, threadIdx.x);
