bc9bd1255167aacd12275624001b2cc9689c776c
Thomas Maranzatto <thomas.maranzatto16@ncf.edu>
2019-07-15 10:44:00 -0400

    working on array class

--- array/kernel.cu
+++ array/kernel.cu
@@ -3,39 +3,34 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
-#include <iostream>
 
 //returns value for threadId%64 if not taken
 //else returns NULL
 //assumes array is for integers for simplicity
 //can template this later
 
-struct arrayNode {
-	int data[64];
-	unsigned long long int bitmap;
-	int lo;
-	int hi;
-	arrayNode *next;
-};
-__global__
-void get(arrayNode *k){
-	unsigned long long int bitmap = k->bitmap;
-	int *arr = k->data;
 
-	//defining primes to prevent locking/atomics issues
+
+__global__
+void get(int* array, unsigned long long int* bitmap) {
+	printf("your thread is %d.%d.\n", blockIdx.x * blockDim.x, threadIdx.x);
+
 	const int resolution = 8;
 	int jumps[resolution] = { 3, 5, 7, 11, 13, 17, 19, 23 };
-
 	//Loop through all 64 elements to see if we find one that works
 	//hopefully our thdId gives us a result immediatly
 	for(int k = 0; k < 64; k++){
 		int flag = 0;
+		int test = 0;
 		//checking edge case that the array is empty
 		//send a message to host
-		if (bitmap == ULLONG_MAX) {
+		if (*bitmap == ULLONG_MAX) {
 			//do something
+			
 			flag = -1;
-
+			#if __CUDA_ARCH__ >= 200
+				printf("%d\n", flag);
+			#endif
 		}
 		int jump = jumps[threadIdx.x % resolution];
 		unsigned long long int i = (blockIdx.x * blockDim.x + threadIdx.x + jump) % 64;
@@ -45,29 +40,41 @@ void get(arrayNode *k){
 		if ((previousValue >> i) & 1 == 0) {
 			//do something with the value
 			flag = 1;
-
+			#if __CUDA_ARCH__ >= 200
+						printf("%d\n", flag);
+			#endif
 		}
 
 		else{
 			//else try next element
-
 			continue;
+			#if __CUDA_ARCH__ >= 200
+				printf("try again\n");
+			#endif
 		}
 
 	}
 }
 
 int main(void) {
-	arrayNode *k;
-	cudaMallocManaged(&k, sizeof(k));
+	unsigned long long int *bitmap;
+	int *arr;
+
+	cudaMallocManaged(&bitmap, sizeof(unsigned long long int));
+	cudaMallocManaged(&arr, 64*sizeof(int));
 
 	//dumb init of array
 	for (int i = 0; i < 64; i++) {
-		k->data[i] = i;
+		arr[i] = i;
 	}
+	printf("processing...\n");
 
-	get<<<1, 256 >>>(k);
+	get<<<1, 16>>>(arr, bitmap);
+	cudaDeviceSynchronize();
 
-	cudaFree(k);
+	printf("task complete.\n");
+	cudaFree(bitmap);
+	cudaFree(arr);
+
 
 }
